bff8e905f0da10ca5cbae3b5d89273e8e5363937
RAFA1211 <rafael.angarita.raar@gmail.com>
2021-09-01 14:38:51 -0500

    cambios1sep

--- main.js
+++ main.js
@@ -100,42 +100,42 @@ obj [0][1] = "Rafael Angarita";
             let obj ={
                 "Datos_animales": data3,
             "Datos_Peces": data4 };
-    obj.Datos_Animales[0] = "Leon";
-            console.log(obj.Datos_Animales);})
+    obj.Datos_animales[2] = "Leon";
+            console.log(obj.Datos_animales);})
 
 
-// / let obj = {datos:matriz};
-// // Object.preventExtensions(obj);
-// // obj.datos[3][2][4][0][0] = "Miguel";
-// // console.log(obj);
+// let objeto = {datos:matriz};
+// Object.preventExtensions(objeto);
+// objeto.datos[3][0][4][0][0] = "Rafael Angarita";
+// console.log(objeto);
 
-// let D = 5;
-// let matriz = new Array(D).fill(null);
+let D = 5;
+let matriz4 = new Array(D).fill(null);
 
-// for(var i=0; i<matriz.length; i++) {
-//     matriz[i] = new Array(D).fill(null);
-//     for(var j=0; j<matriz[i].length; j++) {
-//         matriz[i][j] = new Array(D).fill(null);
-//         for(var r=0; r<matriz[i][j].length; r++) {
-//             matriz[i][j][r] = new Array(D).fill(null);
-//             for(var q=0; q<matriz[i][j][r].length; q++) {
-//                 matriz[i][j][r][q] = new Array(D).fill(null);
-//             }
-//         }
-//     }
-// }
-
-
+for(var i=0; i<matriz.length; i++) {
+    matriz[i] = new Array(D).fill(null);
+    for(var j=0; j<matriz[i].length; j++) {
+        matriz[i][j] = new Array(D).fill(null);
+        for(var r=0; r<matriz[i][j].length; r++) {
+            matriz[i][j][r] = new Array(D).fill(null);
+            for(var q=0; q<matriz[i][j][r].length; q++) {
+                matriz[i][j][r][q] = new Array(D).fill(null);
+            }
+        }
+    }
+}
 
 
-// let obj = {
-//     suma : function(){
-//         return this.num1 + this.num2;
-//     }
-// }
-// let obj2 = Object.create(obj);
-// obj2.num1 = 5;
-// obj2.num2 = 5;
 
-// console.log(obj2);
-// // console.log(obj2.suma());
+
+let obj = {
+    suma : function(){
+        return this.num1 + this.num2;
+    }
+}
+let obj2 = Object.create(obj);
+obj2.num1 = 5;
+obj2.num2 = 5;
+
+console.log(obj2);
+// console.log(obj2.suma());
